abfd7033615a35a68652b607b51f047967d27336
Mate Vasarhelyi <vasarhelyi.mate@gmail.com>
2020-10-02 13:01:02 +0200

    feat(*): empty rating page BIX-0

--- pages/profile/[companyAlias]/index.tsx
+++ pages/profile/[companyAlias]/index.tsx
@@ -2,33 +2,36 @@ import Axios from 'axios';
 import { GetServerSidePropsContext } from 'next';
 import Page from '../../index';
 import { mockData } from '../../../shared/data/mockData';
+import { createNoAuthRouteComponent } from '../../../shared/utils/no-auth-route';
+import { PublicProfile } from '../../../shared/pages/public-profile/public-profile';
+import { createAuthRouteComponent } from '../../../shared/utils/auth-route';
 
-export default Page;
+export default createNoAuthRouteComponent(PublicProfile);
 
-export async function getServerSideProps(
-  context: GetServerSidePropsContext<{ companyAlias: string }>,
-): Promise<unknown> {
-  const host = context?.req?.headers?.host;
+// export async function getServerSideProps(
+//   context: GetServerSidePropsContext<{ companyAlias: string }>,
+// ): Promise<unknown> {
+//   const host = context?.req?.headers?.host;
 
-  const {
-    params: { companyAlias },
-  } = context;
+//   const {
+//     params: { companyAlias },
+//   } = context;
 
-  const instance = Axios.create({ baseURL: `http://${host}/api` });
-  const profile = await instance
-    .get(`/public-profiles/${companyAlias}?by=ALIAS`)
-    .then((res) => res.data)
-    .catch((err) => {
-      // console.log({ err });
-      return null;
-    });
+//   const instance = Axios.create({ baseURL: `http://${host}/api` });
+//   const profile = await instance
+//     .get(`/public-profiles/${companyAlias}?by=ALIAS`)
+//     .then((res) => res.data)
+//     .catch((err) => {
+//       // console.log({ err });
+//       return null;
+//     });
 
-  console.log({ profile });
+//   console.log({ profile });
 
-  return {
-    props: {
-      publicProfile: mockData(),
-      profile,
-    }, // will be passed to the page component as props
-  };
-}
+//   return {
+//     props: {
+//       publicProfile: mockData(),
+//       profile,
+//     }, // will be passed to the page component as props
+//   };
+// }
---
+++ shared/pages/rating/rating.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+interface RatingProps {
+  form: any;
+}
+
+export const Rating: FC<RatingProps> = (props) => {
+  return <div></div>;
+};
