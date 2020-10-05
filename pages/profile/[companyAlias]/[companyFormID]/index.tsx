e31859ae2178a568d89ae8c9b3422dc4b7321169
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2020-10-05 11:18:13 +0200

    feat(*): remove route BIX-0

--- pages/profile/[companyAlias]/[companyFormID]/index.tsx
+++
@@ -1,4 +0,0 @@
-import { createAuthRouteComponent } from '../../../../shared/utils/auth-route';
-import { Rating } from '../../../../shared/pages/rating/rating';
-
-export default createAuthRouteComponent(Rating);
