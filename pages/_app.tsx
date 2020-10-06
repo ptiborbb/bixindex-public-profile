83b914c8b0dea726c129f230e9aec8a49134d83a
Gergely Veres <gergely424@gmail.com>
2020-10-06 07:47:14 +0200

    feat: login page public prof BIX-303

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ import { TranslateContext } from '../shared/translate.context';
 import { authServiceFactory } from '../shared/services/auth.service';
 import { publicProfileServiceFactory } from '../shared/services/public-profile.service';
 import { ratingServiceFactory } from '../shared/services/rating.service';
+import createPalette from '@material-ui/core/styles/createPalette';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 const BixIndexPublicProfile = ({ Component, pageProps }: AppProps): JSX.Element => {
   const [state, dispatch] = useReducer<AppReducer>(appReducer, {
@@ -25,22 +27,63 @@ const BixIndexPublicProfile = ({ Component, pageProps }: AppProps): JSX.Element 
   const publicProfileService = useMemo(() => publicProfileServiceFactory(bixClient, dispatch), [bixClient]);
   const ratingService = useMemo(() => ratingServiceFactory(bixClient, dispatch), [bixClient]);
 
+  const theme = createMuiTheme({
+    props: {
+      MuiCard: {
+        elevation: 4,
+      },
+      MuiAccordion: {
+        elevation: 4,
+      },
+      MuiTextField: {
+        InputLabelProps: {
+          shrink: true,
+        },
+        variant: 'outlined',
+      },
+      MuiButton: {
+        variant: 'contained',
+        color: 'secondary',
+        style: {
+          padding: '15px',
+          width: '17em',
+        },
+      },
+    },
+    typography: {
+      button: {
+        textTransform: 'none',
+      },
+    },
+    palette: createPalette({
+      primary: {
+        main: '#5D92F4',
+      },
+      secondary: {
+        main: '#00D014',
+        contrastText: '#FFFFFF',
+      },
+    }),
+  });
+
   return (
-    <AppContext.Provider
-      value={{
-        state,
-        dispatch,
-        authService,
-        publicProfileService,
-        ratingService,
-      }}
-    >
-      <ContextDevTool context={AppContext} id="bixPublicProfile" displayName="Bix Public Profile" />
-      <TranslateContext.Provider value={{ i18n, t }}>
-        <Component {...pageProps} />
-        <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} hideProgressBar={true} />
-      </TranslateContext.Provider>
-    </AppContext.Provider>
+    <ThemeProvider theme={theme}>
+      <AppContext.Provider
+        value={{
+          state,
+          dispatch,
+          authService,
+          publicProfileService,
+          ratingService,
+        }}
+      >
+        <ContextDevTool context={AppContext} id="bixPublicProfile" displayName="Bix Public Profile" />
+        <TranslateContext.Provider value={{ i18n, t }}>
+          <Component {...pageProps} />
+          <ToastContainer position={toast.POSITION.BOTTOM_RIGHT} hideProgressBar={true} />
+        </TranslateContext.Provider>
+      </AppContext.Provider>
+    </ThemeProvider>
   );
 };
 
--- shared/pages/auth/auth.tsx
+++ shared/pages/auth/auth.tsx
@@ -1,13 +1,18 @@
-import { Button, FormControl, FormHelperText } from '@material-ui/core';
+import { Button, FormControl, FormHelperText, Grid, InputAdornment, Paper } from '@material-ui/core';
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { TextField } from 'formik-material-ui';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FunctionComponent, useCallback, useState } from 'react';
 import * as Yup from 'yup';
 import { useApp } from '../../app.context';
+import { Header } from '../../components/header/header';
 import { useTranslate } from '../../translate.context';
 import classes from './auth.module.scss';
+import logo from '../../../public/bix_logo.svg';
+import Head from 'next/head';
+import MailIcon from '@material-ui/icons/Mail';
+import PersonIcon from '@material-ui/icons/Person';
+import LockIcon from '@material-ui/icons/Lock';
 
 export const Auth: FunctionComponent = () => {
   const { t } = useTranslate();
@@ -64,84 +69,187 @@ export const Auth: FunctionComponent = () => {
 
   return (
     <div>
-      <div>
-        <h2 className={classes.title}>Login</h2>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validationSchema={loginValidationSchema}
-          onSubmit={(values: ILoginFormValues, { setSubmitting, resetForm }: FormikHelpers<ILoginFormValues>) => {
-            return login(values.email, values.password).then(() => {
-              setSubmitting(false);
-              resetForm();
-            });
-          }}
-        >
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+        />
+      </Head>
+      <>
+        <div className={classes.headerBlock}>
           <div className={classes.container}>
-            <Form className={classes.form} noValidate>
-              <FormControl error={error.isError}>
-                <Field id="email" name="email" label={t('AUTH.EMAIL')} component={TextField} fullWidth />
-                <Field
-                  id="password"
-                  type="password"
-                  name="password"
-                  label={t('AUTH.PASSWORD')}
-                  component={TextField}
-                  fullWidth
-                />
-                <FormHelperText>{error.message}</FormHelperText>
-              </FormControl>
-              <div className={classes.button}>
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                  {t('AUTH.LOGIN')}
-                </Button>
-              </div>
-            </Form>
+            <Header logoPath={logo} />
           </div>
-        </Formik>
-      </div>
-      <div>
-        <h2 className={classes.title}>Register</h2>
-        <Formik
-          initialValues={{
-            name: '',
-            email: '',
-            password: '',
-          }}
-          validationSchema={registerValidationSchema}
-          onSubmit={(values: IRegisterFormValues, { setSubmitting, resetForm }: FormikHelpers<IRegisterFormValues>) => {
-            return register(values.name, values.email, values.password).then(() => {
-              setSubmitting(false);
-              resetForm();
-            });
-          }}
-        >
-          <div className={classes.container}>
-            <Form className={classes.form} noValidate>
-              <FormControl error={error.isError}>
-                <Field id="name" name="name" label={t('AUTH.NAME')} component={TextField} fullWidth />
-                <Field id="email" name="email" label={t('AUTH.EMAIL')} component={TextField} fullWidth />
-                <Field
-                  id="password"
-                  type="password"
-                  name="password"
-                  label={t('AUTH.PASSWORD')}
-                  component={TextField}
-                  fullWidth
-                />
-                <FormHelperText>{error.message}</FormHelperText>
-              </FormControl>
-              <div className={classes.button}>
-                <Button type="submit" variant="contained" color="primary" fullWidth>
-                  {t('AUTH.REGISTER')}
-                </Button>
-              </div>
-            </Form>
-          </div>
-        </Formik>
-      </div>
+          <div className={classes.divider}></div>
+        </div>
+        <div className={classes.container}>
+          <h1 className={classes.pageTitle}>Azonosítás</h1>
+          <Grid container justify="center" spacing={3}>
+            <Grid item xs md={5}>
+              <Paper className={classes.paper}>
+                <h2 className={classes.title}>
+                  <span>Nincs</span> még BIX accountod?
+                </h2>
+                <div className={classes.description}>
+                  <span>Azonosítás e-mail címmel</span>
+                </div>
+                <Formik
+                  initialValues={{
+                    name: '',
+                    email: '',
+                    password: '',
+                  }}
+                  validationSchema={registerValidationSchema}
+                  onSubmit={(
+                    values: IRegisterFormValues,
+                    { setSubmitting, resetForm }: FormikHelpers<IRegisterFormValues>,
+                  ) => {
+                    return register(values.name, values.email, values.password).then(() => {
+                      setSubmitting(false);
+                      resetForm();
+                    });
+                  }}
+                >
+                  <Form className={classes.form} noValidate>
+                    <FormControl error={error.isError} fullWidth>
+                      <Field
+                        id="name"
+                        name="name"
+                        label={t('AUTH.NAME')}
+                        component={TextField}
+                        fullWidth
+                        // variant="outlined"
+                        // className={classes.formInput}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <PersonIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      <Field
+                        id="email"
+                        name="email"
+                        label={t('AUTH.EMAIL')}
+                        component={TextField}
+                        fullWidth
+                        // variant="outlined"
+                        // className={classes.formInput}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <MailIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      <Field
+                        id="password"
+                        type="password"
+                        name="password"
+                        label={t('AUTH.PASSWORD')}
+                        component={TextField}
+                        fullWidth
+                        // variant="outlined"
+                        // className={classes.formInput}
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <LockIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                      <FormHelperText>{error.message}</FormHelperText>
+                    </FormControl>
+                    <div className={classes.button}>
+                      <Button type="submit" variant="contained" color="secondary" fullWidth>
+                        {t('AUTH.REGISTER')}
+                      </Button>
+                    </div>
+                  </Form>
+                </Formik>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <Paper className={classes.paper}>
+                <Grid container justify="center" spacing={3}>
+                  <h2 className={classes.title}>
+                    <span>Van</span> már BIX accountod?
+                  </h2>
+                  <div className={classes.description}>
+                    <span>Belépés e-mail címmel</span>
+                  </div>
+                  <Formik
+                    initialValues={{
+                      email: '',
+                      password: '',
+                    }}
+                    validationSchema={loginValidationSchema}
+                    onSubmit={(
+                      values: ILoginFormValues,
+                      { setSubmitting, resetForm }: FormikHelpers<ILoginFormValues>,
+                    ) => {
+                      return login(values.email, values.password).then(() => {
+                        setSubmitting(false);
+                        resetForm();
+                      });
+                    }}
+                  >
+                    <Form className={classes.form} noValidate>
+                      <FormControl error={error.isError} fullWidth>
+                        <Field
+                          id="email"
+                          name="email"
+                          label={t('AUTH.EMAIL')}
+                          component={TextField}
+                          fullWidth
+                          // variant="outlined"
+                          // className={classes.formInput}
+                          InputProps={{
+                            shrink: 'true',
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <MailIcon />
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+
+                        <Field
+                          id="password"
+                          type="password"
+                          name="password"
+                          label={t('AUTH.PASSWORD')}
+                          component={TextField}
+                          fullWidth
+                          // className={classes.formInput}
+                          InputProps={{
+                            shrink: 'true',
+                            startAdornment: (
+                              <InputAdornment position="start">
+                                <LockIcon />
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                        <FormHelperText>{error.message}</FormHelperText>
+                      </FormControl>
+                      <div className={classes.button}>
+                        <Button type="submit" variant="contained" color="secondary" fullWidth>
+                          {t('AUTH.LOGIN')}
+                        </Button>
+                      </div>
+                    </Form>
+                  </Formik>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
+      </>
     </div>
   );
 };
