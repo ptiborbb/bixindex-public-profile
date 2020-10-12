51da184f851b80db79cc551ac3920a621c8ba597
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2020-10-12 15:38:48 +0200

    feat(profile-list): add spinner BIX-0

--- shared/pages/profile-list/store/reducer.ts
+++ shared/pages/profile-list/store/reducer.ts
@@ -6,16 +6,21 @@ export const profileListReducer = createReducer(initialProfileListState, (builde
     .addCase(getProfiles, (state, action) => {
       state.page = action.payload.page;
       state.rowsPerPage = action.payload.rowsPerPage;
+      state.loading = true;
+      state.profiles = initialProfileListState.profiles;
     })
     .addCase(getProfilesSuccess, (state, action) => {
       state.profiles = action.payload.items;
       state.count = action.payload.count;
+      state.loading = false;
     })
     .addCase(getProfilesFail, (state) => {
       state.profiles = null;
+      state.loading = false;
     })
     .addCase(resetProfileList, (state) => {
       state.profiles = initialProfileListState.profiles;
+      state.loading = initialProfileListState.loading;
     })
     .addDefaultCase((state) => state);
 });
