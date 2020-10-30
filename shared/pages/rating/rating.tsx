085d41a404964ba016a74b98e5b7733c483acad5
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2020-10-30 12:19:25 +0100

    fix(rating): fix validation BIX-446

--- shared/pages/rating/rating.tsx
+++ shared/pages/rating/rating.tsx
@@ -112,7 +112,7 @@ export const Rating: FC = () => {
             comment: Yup.string().required(t('COMMON.REQUIRED')),
             auth: authValidation,
           }),
-    [nps, authValidation],
+    [nps, authValidation, user],
   );
 
   const loginResponseFacebook = useCallback(
@@ -284,7 +284,7 @@ export const Rating: FC = () => {
                     <Formik
                       initialValues={{
                         satisfaction: '',
-                        nps: 4,
+                        nps: 8,
                         answers: companyForm.questions,
                         positive: '',
                         negative: '',
@@ -308,7 +308,7 @@ export const Rating: FC = () => {
                       validationSchema={validationSchema}
                       enableReinitialize
                     >
-                      {({ setFieldValue, errors, submitCount, values, isValid, submitForm }) => (
+                      {({ setFieldValue, errors, submitCount, values, isValid, submitForm, validateForm }) => (
                         <Form style={{ width: '100%' }}>
                           {!nps && (
                             <>
@@ -356,7 +356,7 @@ export const Rating: FC = () => {
                             <Grid container spacing={0} justify="space-between">
                               <Grid item xs={12}>
                                 <CustomSlider
-                                  defaultValue={8}
+                                  defaultValue={values.nps}
                                   name="nps"
                                   valueLabelDisplay="auto"
                                   step={1}
@@ -518,7 +518,10 @@ export const Rating: FC = () => {
                                       appId={fbAppId}
                                       autoLoad={false}
                                       fields="name,email,picture"
-                                      callback={loginResponseFacebook}
+                                      callback={async (response) => {
+                                        await loginResponseFacebook(response);
+                                        await validateForm();
+                                      }}
                                       render={(renderProps) => (
                                         <button
                                           className={`${classes.socialButton} ${classes.facebook}`}
@@ -547,9 +550,13 @@ export const Rating: FC = () => {
                                           {t('AUTH.GOOGLE_BTN_TEXT')}
                                         </button>
                                       )}
-                                      onSuccess={(resp: GoogleLoginResponse) =>
-                                        responseGoogle(resp, values.auth.loginOrRegister === ELoginOrRegister.REGISTER)
-                                      }
+                                      onSuccess={async (resp: GoogleLoginResponse) => {
+                                        await responseGoogle(
+                                          resp,
+                                          values.auth.loginOrRegister === ELoginOrRegister.REGISTER,
+                                        );
+                                        await validateForm();
+                                      }}
                                       onFailure={failResponseGoogle}
                                       cookiePolicy={'single_host_origin'}
                                     />
@@ -716,6 +723,7 @@ export const Rating: FC = () => {
                               variant="contained"
                               color="primary"
                               onClick={() => {
+                                console.log(errors);
                                 isValid ? submitForm() : toast.error(t('RATING.INVALID_FORM'));
                               }}
                             >
