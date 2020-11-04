605b2c7deb46ef46aa43e289fbd57fb4d265bce0
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2020-11-04 13:23:12 +0100

    feat(product): add image to products BIX-0

--- shared/components/product/product.tsx
+++ shared/components/product/product.tsx
@@ -1,12 +1,16 @@
 import { IProduct, IService } from '@codingsans/bixindex-common';
-import { FC } from 'react';
+import { Avatar } from '@material-ui/core';
+import { Attachment } from '@material-ui/icons';
+import React, { FC, useState } from 'react';
 import { useTranslate } from '../../translate.context';
+import { BixModal } from '../bix-modal/bix-modal';
 import classes from './product.module.scss';
 
 type ProductProps = IProduct & IService;
 
-export const Product: FC<ProductProps> = ({ name, mainCategory, subCategory, pricing, description }) => {
+export const Product: FC<ProductProps> = ({ name, mainCategory, subCategory, priceRange, description }) => {
   const { t } = useTranslate();
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false);
   return (
     <div className={classes.product}>
       <div className={classes.details}>
@@ -18,12 +22,21 @@ export const Product: FC<ProductProps> = ({ name, mainCategory, subCategory, pri
         </div>
         <div className={classes.subDetails}>
           <div className={classes.priceLabel}>Árazás</div>
-          <div className={classes.price}>{pricing}</div>
+          <div className={classes.price}>{priceRange}</div>
         </div>
       </div>
 
       <div className={classes.descriptionLabel}>Leírás</div>
       <div className={classes.description}>{description}</div>
+      <div className={classes.descriptionLabel}>Kép</div>
+      <div className={classes.attachmentWrapper} onClick={() => setIsImageModalOpen(true)}>
+        <Avatar className={classes.attachment} variant="square" src="https://placekitten.com/800/1000">
+          <Attachment fontSize="large" />
+        </Avatar>
+      </div>
+      <BixModal open={isImageModalOpen} onClose={() => setIsImageModalOpen(false)}>
+        <img className={classes.modalImage} src="https://placekitten.com/800/1000" />
+      </BixModal>
     </div>
   );
 };
