196189a6a75fd2f7ac21253862c3de269cb078d1
Kristóf Várszegi <varszegi.kristof99@gmail.com>
2020-11-18 19:58:14 +0100

    fix: type

--- shared/components/fragments/reviews/components/review-stats/review-rate/review-rate.tsx
+++ shared/components/fragments/reviews/components/review-stats/review-rate/review-rate.tsx
@@ -1,12 +1,10 @@
-import { SvgIconTypeMap } from '@material-ui/core';
-import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 import { FC, useMemo } from 'react';
 import classes from './review-rate.module.scss';
 
 export interface ReviewRateProps {
   ratingCount: number;
   rating: number;
-  emoji: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>;
+  emoji: JSX.Element;
   label: string;
 }
 
