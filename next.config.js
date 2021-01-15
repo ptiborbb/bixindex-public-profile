efd0a7b9c4a222a0510b85385d87edb4ecab3386
LaTotty <latotty91@gmail.com>
2021-01-15 14:08:06 +0100

    feat: vercel - next rewrite
    BIX-970

--- next.config.js
+++ next.config.js
@@ -2,12 +2,26 @@ const withImages = require('next-images');
 
 const { nextI18NextRewrites } = require('next-i18next/rewrites');
 
-const localeSubpaths = {};
+const withNextI18NextRewrites = ({ localeSubpaths = {}, ...nextConfig } = {}) => {
+  return Object.assign({}, nextConfig, {
+    rewrites: async () => [
+      ...(nextConfig.rewrites ? await nextConfig.rewrites() : []),
+      ...nextI18NextRewrites(localeSubpaths)
+    ],
+    publicRuntimeConfig: Object.assign({}, nextConfig.publicRuntimeConfig, {
+      localeSubpaths
+    }),
+  });
+}
 
-module.exports = {
-  ...withImages(),
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-};
+module.exports = withNextI18NextRewrites({
+  localeSubpaths: {},
+  ...withImages({
+    rewrites: async () => [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/:path*`,
+      },
+    ],
+  }),
+});
