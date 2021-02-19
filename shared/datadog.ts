8703b6034e7ddcb04f68dc94bdf1fca62355965f
LaTotty <latotty91@gmail.com>
2021-02-19 14:58:50 +0100

    fix: datadog env name

--- shared/datadog.ts
+++ shared/datadog.ts
@@ -2,10 +2,10 @@ import { datadogLogs } from '@datadog/browser-logs';
 
 type DatadogLogsConfigEnvVar = { clientToken: string; site: string; env?: string };
 
-if (typeof window !== undefined && process.env.DATADOG_LOGS_CONFIG) {
+if (typeof window !== undefined && process.env.NEXT_PUBLIC_DATADOG_LOGS_CONFIG) {
   datadogLogs.init({
     service: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG || undefined,
     version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || undefined,
-    ...(JSON.parse(process.env.DATADOG_LOGS_CONFIG) as DatadogLogsConfigEnvVar),
+    ...(JSON.parse(process.env.NEXT_PUBLIC_DATADOG_LOGS_CONFIG) as DatadogLogsConfigEnvVar),
   });
 }
