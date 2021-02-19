8d87d51ead629221a29d33113bc4d51125187667
LaTotty <latotty91@gmail.com>
2021-02-19 15:08:29 +0100

    feat: datadog config

--- shared/datadog.ts
+++ shared/datadog.ts
@@ -6,6 +6,9 @@ if (typeof window !== undefined && process.env.NEXT_PUBLIC_DATADOG_LOGS_CONFIG) 
   datadogLogs.init({
     service: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG || undefined,
     version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA || undefined,
+    forwardErrorsToLogs: true,
     ...(JSON.parse(process.env.NEXT_PUBLIC_DATADOG_LOGS_CONFIG) as DatadogLogsConfigEnvVar),
   });
 }
+
+datadogLogs.logger.info('loaded');
